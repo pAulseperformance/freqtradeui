0fa1bd597f4f0d521002141db80287f737d6c4f9
Paul D. Mendes <pauldmendes5@gmail.com>
2019-10-15 00:59:51 +0300

    Some basic chart functionality with pygal, but will look into plotly dash or bokeh for future mods.

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def index():
             # if command == 'daily':
             #     response = parse_daily(response)
 
-            response = graph_pygal(response)
+            response = daily_performance(response)
             return render_template('index.html', graphdata=response)
             # return render_template('index.html', data=response)
             # return redirect(url_for('index'))
@@ -38,7 +38,7 @@ def index():
 def graph_pygal(response):
     import pygal
     bar_chart = pygal.Bar()
-    bar_chart.title = 'Browser usage evolution (in %)'
+    bar_chart.title = 'Daily Performance'
     bar_chart.x_labels = map(str, range(2002, 2013))
     bar_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
     bar_chart.add('Chrome', [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
@@ -46,6 +46,21 @@ def graph_pygal(response):
     bar_chart.add('Others', [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
     return bar_chart
     # return render_template("graphing.html", graph_data=graph_data)
+
+def daily_performance(response):
+    data = [['2019-10-12', '1.00000000 USDT', '0.000 USD', '0 trade'], ['2019-10-11', '200.00000000 USDT', '0.000 USD', '0 trade'], ['2019-10-10', '-150.00000000 USDT', '0.000 USD', '0 trade'], ['2019-10-09', '25.00000000 USDT', '0.000 USD', '0 trade'], ['2019-10-08', '22.00000000 USDT', '0.000 USD', '0 trade'], ['2019-10-07', '2.00000000 USDT', '0.000 USD', '0 trade'], ['2019-10-06', '12.00000000 USDT', '0.000 USD', '0 trade']]
+    import pygal
+    bar_chart = pygal.Bar()
+    bar_chart.title = 'Daily Performance'
+    print(map(str, range(2002,2013)))
+    bar_chart.x_labels = map(str, [i[0] for i in data])
+    bar_chart.add('USDT', [float(i[1].split(' ')[0]) for i in data])
+    # bar_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    # bar_chart.add('Chrome', [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
+    # bar_chart.add('IE', [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    # bar_chart.add('Others', [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
+    return bar_chart
+
 
 
 def parse_daily(response):
